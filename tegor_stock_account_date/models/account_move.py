9373d9a6b92124239bd9bfc86d7ec6dd79620943
Kristian Yussef Rondon <rondonkristian@gmail.com>
2023-03-13 21:36:03 -0400

    [FIX] modificaciones logica modulo fecha de entrada

--- tegor_stock_account_date/models/account_move.py
+++ tegor_stock_account_date/models/account_move.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import fields, models, api
+from datetime import date, timedelta
 from odoo.exceptions import UserError, ValidationError
 
 
@@ -11,28 +12,32 @@ class AccountMove(models.Model):
     date_entry = fields.Date(related='stock_move_id.date_entry', store=True)
 
 
-    @api.model_create_multi
-    def create(self, vals_list):
+    # @api.model_create_multi
+    # def create(self, vals_list):
         
-        res_ids = super(AccountMove, self).create(vals_list)
+    #     res_ids = super(AccountMove, self).create(vals_list)
 
-        for rec in res_ids:
-            if rec.date_entry:
-                rec.date = rec.date_entry
+    #     for rec in res_ids:
+    #         if rec.date_entry:
+    #             rec.date = rec.date_entry
         
-        return res_ids
+    #     return res_ids
 
 
-    def write(self, vals):
-
-        raise ValidationError(date_entry)
-        res = super(AccountMove, self).write(vals)
-
-        for record in self:
-
-            if record.date_entry:
-
-                record.date = record.date_entry
-
-        return res
-        
+    @api.depends('invoice_date', 'company_id')
+    def _compute_date(self):
+        for move in self:
+            if move.date_entry:
+                move.date = move.date_entry
+            else:
+                if not move.invoice_date:
+                    if not move.date:
+                        move.date = fields.Date.context_today(self)
+                    continue
+                accounting_date = move.invoice_date
+                if not move.is_sale_document(include_receipts=True):
+                    accounting_date = move._get_accounting_date(move.invoice_date, move._affect_tax_report())
+                if accounting_date and accounting_date != move.date:
+                    move.date = accounting_date
+                    # might be protected because `_get_accounting_date` requires the `name`
+                self.env.add_to_compute(self._fields['name'], move)
