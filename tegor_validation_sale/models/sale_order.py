0c0589574642ad289a7c16ee4cc5dd6503c16052
Kristian Yussef Rondon <rondonkristian@gmail.com>
2023-07-21 14:48:50 -0400

    [IMP] nuevo modulo para validar ventas

--- tegor_validation_sale/models/sale_order.py
+++ tegor_validation_sale/models/sale_order.py
@@ -14,20 +14,21 @@ class SaleOrder(models.Model):
         for order in self:
             location = order.warehouse_id.lot_stock_id.id
             for line in order.order_line:
-                product = line.product_id
-                ordered_qty = line.product_uom_qty
+                if line.product_id.detailed_type == 'product':
+                    product = line.product_id
+                    ordered_qty = line.product_uom_qty
 
-                
-                stock_available = self.env['stock.quant'].search([
-                    ('product_id','=',product.id),
-                    ('location_id','=',location)])
+                    
+                    stock_available = self.env['stock.quant'].search([
+                        ('product_id','=',product.id),
+                        ('location_id','=',location)])
 
-                quantity_available = 0
-                for record in stock_available:
-                    quantity_available += record.quantity
+                    quantity_available = 0
+                    for record in stock_available:
+                        quantity_available += record.quantity
 
 
-                if ordered_qty > quantity_available:
-                    raise ValidationError(f"No hay suficiente stock disponible para el producto {product.name} en la ubicación de envío.")
+                    if ordered_qty > quantity_available:
+                        raise ValidationError(f"No hay suficiente stock disponible para el producto {product.name} en la ubicación de envío.")
 
         return super(SaleOrder, self).action_confirm()
