7e6f791f612effe79a1a09ec5d02382c25a62d87
Kristian Yussef Rondon <rondonkristian@gmail.com>
2023-07-25 11:53:56 -0400

    [ADD] modificaciones  lote

--- tegor_archive_lot/models/stock_lot.py
+++ tegor_archive_lot/models/stock_lot.py
@@ -9,13 +9,15 @@ class StockLot(models.Model):
 
 
 
-    active = fields.Boolean('Archivo', compute='_compute_campo_bool', store=True)
+    active = fields.Boolean('Archivo', default=True)
+    active_2 = fields.Boolean('Archivo', compute='_compute_campo_bool', store=True)
 
 
     @api.depends('product_qty')
     def _compute_campo_bool(self):
         for record in self:
             if record.product_qty <= 0:
+                record.active_2 = False
                 record.active = False
             else:
                 record.active = True
