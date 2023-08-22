2ad9d37dbf68728305f45e38859dfab14fbda580
Kristian Yussef Rondon <rondonkristian@gmail.com>
2023-08-22 15:33:00 -0400

    [FIX] modificacion validador stock picking

--- tegor_validation_stock/models/stock.py
+++ tegor_validation_stock/models/stock.py
@@ -12,22 +12,24 @@ class StockPicking(models.Model):
 
     def button_validate(self):
         for stock in self:
-            location = stock.location_id.id
-            for line in stock.move_line_ids_without_package:
-                product = line.product_id
-                ordered_qty = line.qty_done
 
-                
-                stock_available = self.env['stock.quant'].search([
-                    ('product_id','=',product.id),
-                    ('location_id','=',location)])
+            if stock.picking_type_code in ['outgoing','internal']:
+                location = stock.location_id.id
+                for line in stock.move_line_ids_without_package:
+                    product = line.product_id
+                    ordered_qty = line.qty_done
 
-                quantity_available = 0
-                for record in stock_available:
-                    quantity_available += record.quantity
+                    
+                    stock_available = self.env['stock.quant'].search([
+                        ('product_id','=',product.id),
+                        ('location_id','=',location)])
+
+                    quantity_available = 0
+                    for record in stock_available:
+                        quantity_available += record.quantity
 
 
-                if ordered_qty > quantity_available:
-                    raise ValidationError(f"No hay suficiente stock disponible para el producto {product.name} en la ubicación de Origen.")
+                    if ordered_qty > quantity_available:
+                        raise ValidationError(f"No hay suficiente stock disponible para el producto {product.name} en la ubicación de Origen.")
 
         return super(StockPicking, self).button_validate()
