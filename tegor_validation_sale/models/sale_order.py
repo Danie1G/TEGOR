7f0340b1415320df07f930a999d81dc54d4af1b1
Kristian Yussef Rondon <rondonkristian@gmail.com>
2023-12-05 18:02:50 -0400

    [FIX] modificacion en la venta

--- tegor_validation_sale/models/sale_order.py
+++ tegor_validation_sale/models/sale_order.py
@@ -15,20 +15,21 @@ class SaleOrder(models.Model):
             location = order.warehouse_id.lot_stock_id.id
             for line in order.order_line:
                 if line.product_id.detailed_type == 'product':
-                    product = line.product_id
-                    ordered_qty = line.product_uom_qty
+                    if not line.product_id.product_tmpl_id.is_kit:
+                        product = line.product_id
+                        ordered_qty = line.product_uom_qty
 
-                    
-                    stock_available = self.env['stock.quant'].search([
-                        ('product_id','=',product.id),
-                        ('location_id','=',location)])
+                        
+                        stock_available = self.env['stock.quant'].search([
+                            ('product_id','=',product.id),
+                            ('location_id','=',location)])
 
-                    quantity_available = 0
-                    for record in stock_available:
-                        quantity_available += record.quantity
+                        quantity_available = 0
+                        for record in stock_available:
+                            quantity_available += record.quantity
 
 
-                    if ordered_qty > quantity_available:
-                        raise ValidationError(f"No hay suficiente stock disponible para el producto {product.name} en la ubicación de envío.")
+                        if ordered_qty > quantity_available:
+                            raise ValidationError(f"No hay suficiente stock disponible para el producto {product.name} en la ubicación de envío.")
 
         return super(SaleOrder, self).action_confirm()
